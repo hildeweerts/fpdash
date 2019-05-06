34f0987d6db4b232a57c58980bee5f52f2417f0b
Hilde Weerts <hilde_weerts@hotmail.com>
2019-05-06 19:58:37 +0200

    Change width of CBR part

--- app.py
+++ app.py
@@ -74,29 +74,6 @@ cat_colors = {'TP' : 'rgba(159, 211, 86, %s)' % opacity,
 COMPUTE SHAP WITH SAMPLES
 """
 
-# # SHAP VALUES
-# def compute_shap(instance, nr_samples, top):
-#     # Compute SHAP values
-#     shap_values = explainer.standard(X_alert.iloc[instance], m = nr_samples, return_samples=True)
-#     # Retrieve most important features
-#     df = pd.DataFrame.from_dict(shap_values.shap_values, orient = 'index').reset_index(level=0)
-#     df = df.reindex(df[0].abs().sort_values(ascending = False).index)
-#     features = list(df['index'].iloc[0:top])
-#     importances = list(df[0].iloc[0:top])
-#     # Retrieve feature value
-#     values = [X_alert_decoded.iloc[instance][f] for f in features]
-#     # Retrieve errors
-#     errors = []
-#     alpha = 0.05
-#     for f in features: 
-#         index = list(X_base).index(f)
-#         samples = shap_values.samples[index]
-#         n = len(samples)
-#         t = stats.t.ppf(1-alpha/2, n)
-#         s = samples['c'].std()
-#         errors.append(t*s/np.sqrt(n))
-#     return importances, features, values, errors
-
 def retrieve_shap(instance, top):
     # Retrieve SHAP values    
     shap_values = SHAP_alert.iloc[instance].to_dict()
@@ -157,78 +134,6 @@ Feature importances
 """
 def generate_options():
     return [{'label' : 'Case %s' % nr, 'value' : nr} for nr in range(1,11)]
-
-# def feature_importance_bar(shap_value, error, lim):
-#     if shap_value >= 0:
-#         color = '#0DB5E6'
-#     else:
-#         color = '#ffa54c'
-#     # Trace definition
-#     hoverlabel = {
-#         'bordercolor' : 'white',
-#         'font' : {'size' : 10},
-#     }
-#     trace = go.Bar(x = [shap_value] , 
-#                y = [''],
-#                orientation = 'h',
-#                hoverinfo = 'x',
-#                hoverlabel = hoverlabel,
-#                marker = {'color' : color},
-#                error_x= {'type' : 'data', 
-#                          'array' : [error],
-#                          'visible' : True,
-#                          'thickness': 1,
-#                          'width' : 2, 
-#                          'color' : '#727272'}
-#     )
-#     # Layout definition
-#     xaxis = {
-#         'range' : [-lim, lim],
-#         'fixedrange' : True,
-#         'showgrid' : False,
-#         'zeroline' : False,
-#         'showline' : False,
-#         'showticklabels' : False,
-#         'hoverformat': '.2f'
-#     }
-#     yaxis = {
-#         'fixedrange' : True,
-#         'showgrid' : False,
-#         'zeroline' : False,
-#         'showline' : False,
-#         'showticklabels' : False
-#     }
-#     margin=go.layout.Margin(l=0, r=0, t=0, b=0, pad=0)
-#     layout = go.Layout(yaxis = yaxis,
-#                        xaxis = xaxis,
-#                        margin = margin,
-#                        bargap = 0)
-    
-#     # Config definition
-#     config={'displayModeBar': False,
-#             'showLink' : False}
-    
-#     return dcc.Graph(figure = {'data' : [trace],
-#                                'layout' : layout}, 
-#                      config = config,
-#                      style = {'height' : '18px',
-#                               'width' : '170px'})
-# def feature_importance_table(importances, features, values, errors):
-#     # Add header
-#     table = [html.Tr([html.Th(col) for col in ['Contribution', 'Feature', 'Value']])]
-#     # Add body
-#     lim = np.abs(importances[0]) + 0.2
-#     for importance, feature, value, error in zip(importances, features, values, errors):
-#         table.append(html.Tr([
-#             html.Td(feature_importance_bar(importance, error, lim)),
-#             html.Td(feature),
-#             html.Td(value),
-#         ]))
-        
-#     return html.Table(table,
-#                       style={'font-size': '1.5rem',
-#                              'marginTop' : '10px'}
-#                      )
 
 def feature_importance_bar_exact(shap_value, lim):
     if shap_value >= 0:
@@ -413,7 +318,7 @@ def scatter_neighbors(x, y, neighbors, view, instance, border_width=4):
             'showticklabels' : False
         }
     margin = go.layout.Margin(l=0, r=0, t=0, b=0, pad=0)
-    layout = go.Layout(yaxis = yaxis, xaxis = xaxis, margin = margin, height = 300,
+    layout = go.Layout(yaxis = yaxis, xaxis = xaxis, margin = margin, height = 400,
                        hovermode = 'closest', legend = dict(y=-0.05, orientation='h'),
                        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)',title='Hoi')
     
@@ -554,63 +459,20 @@ app.layout = html.Div([
                                      ))
                                  ])
                              ]),
-#                     html.Div(className='row', 
-#                              children = [
-#                                  html.H3(['Number of Samples ',
-#                                           html.Div([html.I(className="fas fa-info-circle", style=iconStyle),
-#                                                     html.Span( """Increasing the number of samples per feature results in
-#                                                                    more accurate approximations but increases the 
-#                                                                    computation time.""",
-#                                                                className='tooltiptext')], 
-#                                                     className = "tooltip")])
-#                              ]),
-#                     # Slider
-#                     html.Div(className='row',
-#                              children=[
-#                                  html.Div(className="nine columns",
-#                                           children = [dcc.Slider(
-#                                               id='samples-slider',
-#                                               min=0,
-#                                               max=1000,
-#                                               step=50,
-#                                               value=100,                            
-#                                               marks={str(n): {'label' : str(n), 
-#                                                               'style' : {'font-size' : 10}} for n in range(0,1100,100)})])
-#                                       ]),
-#                     html.Div(className = 'row', 
-#                              children = [html.Button('Compute', 
-#                                                      id='compute-button',
-#                                                      style = {'marginTop' : '2.5rem'})]),
+
                     html.Div(id='load-importances'),
                     html.Div(id='feature-importance-table')
                 ],
-                className="six columns",
+                className="five columns",
                 id = 'explanation',
                 style = middleColumnStyle),
             
             # RIGHT COLUMN: NEIGHBORS
-            html.Div(className="four columns",
+            html.Div(className="five columns",
                      style = columnStyle,
                      children = [html.H1('Case-Based Performance'),
                                  html.H2('Most Similar Cases'),
                                  html.P('The cases most similar to the alert are retrieved.'),
-#                                 html.Div(className = 'row',
-#                                           children = [
-#                                               html.H4(['Local Accuracy ', 
-#                                                        html.Div([html.I(className="fas fa-info-circle", style=iconStyle),
-#                                                                  html.Span("""Local accuracy is the proportion of similar instances classified 
-#                                                          correctly by the algorithm.""",
-#                                                                            className='tooltiptext')], 
-#                                                                 className = "tooltip")
-#                                                       ], id='accuracy'),
-#                                               html.H4(['Local Precision ', 
-#                                                        html.Div([html.I(className="fas fa-info-circle", style=iconStyle),
-#                                                                  html.Span("""Local precision is the proportion of similar instances classified as 
-#                                                          positive by the classifier that were truly positive.""",
-#                                                                            className='tooltiptext')], 
-#                                                                 className = "tooltip")
-#                                                       ], id='precision')
-#                                           ]),
                                  html.Div(className='row',
                                           children = [
                                               html.H6('Number of Cases'),
@@ -621,11 +483,11 @@ app.layout = html.Div([
                                                       children = [dcc.Slider(
                                                           id='neighbors-slider',
                                                           min=10,
-                                                          max=50,
+                                                          max=100,
                                                           step=5,
                                                           value=20,                            
                                                           marks={str(n): {'label' : str(n), 
-                                                                          'style' : {'font-size' : 10}} for n in range(10,60,10)})])
+                                                                          'style' : {'font-size' : 10}} for n in range(10,110,10)})])
                                           ]),
                                  html.Div(className = 'row',
                                           id='view-div',
@@ -699,25 +561,6 @@ def update_probability(instance):
 """
 COMPUTE SHAP VALUE
 """
-
-# @app.callback(Output('load-importances', 'children'),
-#               [Input('compute-button', 'n_clicks'),
-#                Input('selected-instance', 'children')])
-# def update_importances(n_clicks, children):
-#     if (n_clicks or children):
-#         return html.Div([html.P('Loading...')], 
-#                         id='feature-importance-table',
-#                         style = {'marginTop' : '10px'})
-# @app.callback(
-#     Output('feature-importance-table', 'children'),
-#     [Input('compute-button', 'n_clicks'), 
-#      Input('selected-instance', 'children')],
-#     [State('samples-slider', 'value')])
-# def update_importances(n_clicks, instance, nr_samples):
-#     if (n_clicks or instance):
-#         top = 10
-#         importances, features, values, errors = compute_shap(int(instance), nr_samples, top)
-#         return feature_importance_table(importances, features, values, errors)
 
 @app.callback(
     Output('feature-importance-table', 'children'),
@@ -802,35 +645,6 @@ def update_space_button(n_clicks):
     else:
         children = 'View in feature space'
     return children
-
-"""
-update performance measures
-"""
-# @app.callback(
-#     Output('accuracy', 'children'),
-#     [Input('neighbor-dummydiv', 'children')],
-#     [State('accuracy', 'children')])
-# def update_accuracy(neighbors, children):
-#     meta_neighbors = meta_base.iloc[neighbors]
-#     acc = meta_neighbors['performance'].map({'TP' : 1, 'TN' : 1, 'FP' : 0, 'FN' : 0}).mean()
-#     children[0] = 'Local Accuracy: %.2f ' % acc
-#     return children
-
-# @app.callback(
-#     Output('precision', 'children'),
-#     [Input('neighbor-dummydiv', 'children')],
-#     [State('precision', 'children')])
-# def update_accuracy(neighbors, children):
-#     meta_neighbors = meta_base.iloc[neighbors]
-#     tp = len(meta_neighbors[meta_neighbors['performance'] == 'TP'])
-#     fp = len(meta_neighbors[meta_neighbors['performance'] == 'FP'])
-#     if (tp + fp) == 0:
-#         text = 'Local Precision: NA '
-#     else:
-#         pre = tp/(tp + fp)
-#         text = 'Local Precision: %.2f ' % pre
-#     children[0] = text
-#     return children
 
 if __name__ == '__main__':
     app.run_server(debug=True, processes=4)
